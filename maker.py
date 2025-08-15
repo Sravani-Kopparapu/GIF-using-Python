66ddc3249493311d6387d4b4ec389d045e4ea0b7
Sravani-Kopparapu <kopparapusravani2005@gmail.com>
2025-08-15 22:57:47 +0530

    Update maker.py

--- maker.py
+++ maker.py
@@ -1,6 +1,7 @@
 import imageio as iio
-files = ['nyan-cat1.png', 'nyan-cat2.png', 'nyan-cat3.png']
+files = ['nyan-cat1.png', 'nyan-cat2.png', 'nyan-cat3.png'] # we can also use cat1.png and cat2.png
 images = []
 for file in files:
     images.append(iio.imread(file))
+
 iio.mimsave('cat.gif', images, duration=0.5,loop=0)
